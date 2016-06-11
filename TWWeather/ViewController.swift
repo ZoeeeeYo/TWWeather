832077848ee96cf06440b49cc2f9a31b4c5d7fc0
ZoeeeeYo <apple@Zoes-MacBook-Pro.local>
2016-06-11 15:41:30 +1000

    Initial commit

---
+++ TWWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TWWeather
+//
+//  Created by Zoe on 11/06/2016.
+//  Copyright © 2016 Tingting Wen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
